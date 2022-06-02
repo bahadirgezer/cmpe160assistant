8cfa9fa153b7c0aa54667aaa8bdacce871a5631c
Bahadır Gezer <bahadirgezerbg@gmail.com>
2022-06-02 16:41:33 +0300

    rounded total grades in combined grades. Combiner now prints all grades.

--- grading/grader/submissions/combiner.py
+++ grading/grader/submissions/combiner.py
@@ -5,6 +5,7 @@ if __name__ == "__main__":
     users_dir = os.getcwd()+f"/submitted-graded"
     users = [item for item in os.listdir(users_dir) if os.path.isdir(os.path.join(users_dir, item))]
 
+    totals = dict()
     #javadoc
     for user in users:
         javadoc_path = os.getcwd()+f"/all-grades/javadoc-grades/{user}_javadoc.txt"
@@ -35,10 +36,12 @@ if __name__ == "__main__":
             test_cases_total = polished_file.readline()
             polished_grade = float(polished_file.readline())
 
+        totals.update({user:round(polished_grade + class_structure_grade + compile_grade + javadoc_grade, 2)})
+
+        '''
         combined_file = open(combined_path, "w")
         for i in range(35):
             combined_file.write(inputs[i] + '\n')
-
         combined_file.write(test_cases_total + '\n')
         combined_file.write("javadoc grade: " + str(javadoc_grade) + "/10" + '\n')
         combined_file.write("compile grade: " + str(compile_grade) + "/10" + '\n')
@@ -50,5 +53,7 @@ if __name__ == "__main__":
         combined_file.write(javadoc_rubric + '\n')
         combined_file.write(compile_rubric+ '\n')
         combined_file.write(class_structure_rubric + '\n')
-        combined_file.write("total grade: " + str(polished_grade + class_structure_grade + compile_grade + javadoc_grade)  + "/100" + '\n')
-
+        combined_file.write("total grade: " + str(round(polished_grade + class_structure_grade + compile_grade + javadoc_grade, 2))  + "/100" + '\n')
+        '''
+    
+    for user, grade in sorted(totals.items(), key=lambda x:x[1], reverse=True): print(f"{user}:".rjust(20, ' ') + f"{str(format(round(grade, 2), '.2f')).rjust(8, ' ')}")
